f15be071fd026c0e501ef195ec443d410be2b84e
Eva Larumbe <evalarumbe@gmail.com>
2020-11-16 17:56:03 +0100

    Fix: xkcd 859

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          (This is a sample website. You'll be building a site like this on{' '}<a href="https://nextjs.org/learn">our Next.js tutorial</a>.
+          (This is a sample website. You'll be building a site like this on{' '}<a href="https://nextjs.org/learn">our Next.js tutorial</a>).
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
